8789e4ba01f9fb19ea177e1aaaf40325c41b8b30
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2021-04-21 21:08:11 +0900

    割り当ての許可

--- sample/src/app/Comment.php
+++ sample/src/app/Comment.php
@@ -4,9 +4,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    /**
-     *
-     */
+  protected $fillable = [
+    'post_id',
+    'name',
+    'comment',
+
+  ];
+
     public function post()
     {
         return $this->belongsTo('App\Post');
