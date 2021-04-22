e1b13e6f06a65dc35053f4b50203ef32e238484a
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2021-04-22 18:08:31 +0900

    viewの修正

--- sample/src/resources/views/layouts/bbsheader.blade.php
+++ sample/src/resources/views/layouts/bbsheader.blade.php
@@ -3,17 +3,17 @@
     <div class="container">
         <a class="navbar-brand" href="{{ url('/bbs/') }}">LaravelPjt BBS</a>
         @if (Route::has('login'))
-                <div class="top-right links px-5">
+                <div class="top-right links px-5 py-3">
                     @auth
-<li class="nav-item dropdown">
+<ul class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle text-white bg-dark" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                      onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -21,7 +21,7 @@
                                         @csrf
                                     </form>
                                 </div>
-                            </li>
+                            </ul>
                           @else
                         <a href="{{ route('login') }}" class="text-light">Login</a>
 
