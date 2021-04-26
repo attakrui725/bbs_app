8a8df45c9d3300333d268840c478a7c9030bc547
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2021-04-26 22:30:37 +0900

    フォームを見えなくする #11

--- sample/src/resources/views/auth/register.blade.php
+++ sample/src/resources/views/auth/register.blade.php
@@ -47,11 +47,11 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row" style="display:none;">
                             <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
 
                             <div class="col-md-6">
-                                <input id="role" type="text" class="form-control @error('role') is-invalid @enderror" name="role" value="{{ old('role') }}" required autocomplete="role">
+                                <input id="role" type="text" class="form-control @error('role') is-invalid @enderror" name="role" value="{{ old('role','0') }}" required autocomplete="role" >
 
                                 @error('role')
                                     <span class="invalid-feedback" role="alert">
